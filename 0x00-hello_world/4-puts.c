2cc87698f84ab9631edd053872aa2d65dca83b69
Ben <gugben7@gmail.com>
2023-08-10 10:17:27 -0700

    print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,8 @@
 /*functin prints output*/
 /*funtion always return 0 */
 int main(void)
-
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
 	
 }
