8762986558db4d99b337860d11ad263f3d59bb6a
Ben <gugben7@gmail.com>
2023-08-11 12:09:45 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stadio.h>
+#include <ctype.h>
+
+/**
+ * main- function prints out lowercase of input word
+ * Return:0
+ **/
+
+int main(void)
+{
+	int letter;
+
+	for ( letter = 'a'; letter <= 'z'; letter++)
+	{
+		letter = tolower(letter);
+		putchar(letter);
+
+	}
+
+	putchar('\n');
+	return (0);
+}
