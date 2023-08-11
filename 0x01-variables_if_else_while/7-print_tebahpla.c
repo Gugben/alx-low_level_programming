206cb9642eee70a62c321e60d700deed0a3351f4
Ben <gugben7@gmail.com>
2023-08-11 15:04:35 -0700

    7-print_numberz.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main- function prints out lowercase of input word
+ * Return:0
+ **/
+
+int main(void)
+{
+	int letter;
+
+	for (letter = 'z'; letter => 'a'; letter--)
+
+	{
+		letter = tolower(letter);
+		putchar(letter);
+	}
+
+	putchar('\n');
+	return (0);
+}
