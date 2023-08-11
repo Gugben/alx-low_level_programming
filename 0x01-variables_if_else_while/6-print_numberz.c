174695470f4db71729981fbc5f4460ed8db5c86d
Ben <gugben7@gmail.com>
2023-08-11 14:46:30 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- function prints out base turn sinle digits numbers
+ * Return:0
+ **/
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
