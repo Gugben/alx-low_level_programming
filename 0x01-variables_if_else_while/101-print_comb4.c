601794da5361ac96dbeff116a83df62cb96aa91b
Ben <gugben7@gmail.com>
2023-08-11 19:50:16 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,24 +11,24 @@ int main(void)
 	int n;
 	int c;
 	int b;
-	
+
 	for (n = 0; n <= 9; n++)
 	{
 	/*printf("n:%d ", n);*/
 	for (b = n + 1; b <= 9; b++)
 	{
-	/*printf("b:%d ", b);*/
+
 
 	for (c =  b + 1; c <= 9; c++)
 	{
-	putchar(n);
-	putchar(b);
-	putchar(c);
-	
+	putchar((n % 10) + '0');
+	putchar((b % 10) + '0');
+	putchar(c % 10) + '0');
+
 
 
 	if (n == 7 && b == 8 && c == 9)
-	
+
 	continue;
 
 	putchar(',');
