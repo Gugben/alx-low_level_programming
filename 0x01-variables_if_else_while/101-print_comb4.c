53f5ad89ba2e6369e49e90fd280269b169f10812
Ben <gugben7@gmail.com>
2023-08-11 18:04:22 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main- function prints out 3 single digits number combinations
+ * Return:0
+ **/
+
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '8'; n++)
+	{
+	int b = n + 1;
+
+	for (; b <= '9'; b++)
+	{
+	int c =  b + 1;
+
+	for (; c <= 9; c++)
+	{
+
+	putchar(n);
+	putchar(b);
+	if (n == '7')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
