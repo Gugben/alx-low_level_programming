43a2d9d21c43fda5e39f5020f32321471414bb2c
Ben <gugben7@gmail.com>
2023-08-15 14:56:56 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_abs() - convert integer to absolute value
+ *@n: character to be verified
+ *Return: return absolute value of n
+*/
+int _abs(int n)
+{
+if (n >= 0)
+return (n);
+return (-n);
+}
+
