35f71dc1d314bc83cae477a7c68f2729a87a9a99
Ben <gugben7@gmail.com>
2023-08-15 15:22:37 -0700

    7-last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_last_digit -  return last digit of n
+ *@n: character to be processed
+ *Return: return last digit of n
+*/
+int print_last_digit(int n)
+{
+return (n % 10);
+}          
