b97374e085468660f44f3d21f1b591b4a324503e
Ben <gugben7@gmail.com>
2023-08-15 14:32:14 -0700

    5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- *_isalpha - controls if a character is alphabetical
- *@c: character to be verified
- *Return: return 0 or 1
+ *print_sign- controls if a character is alphabetical
+ *@n: character to be processed
+ *Return: return 0,1 and -1
 */
 int print_sign(int n)
 {
