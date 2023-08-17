a66357ff2dc9030d5765f64ba1c5a378034a2f05
Ben <gugben7@gmail.com>
2023-08-16 23:46:49 -0700

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+/**
+ * _isupper - check if l is uppercase
+ * @l: input alphabet to be checked
+ * Return: 1 if its uppercase, 0 if not
+*/
+
+int _isupper(int c)
+{
+	if (l >= 'a' && l <= 'z')
+		return (1);
+	else
+		return (0);
+}
