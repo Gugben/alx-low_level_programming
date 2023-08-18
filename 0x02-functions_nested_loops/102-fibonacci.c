74b26b4e9b128e3ac5b3c4de0f1a12737131081b
Ben <gugben7@gmail.com>
2023-08-17 17:09:35 -0700

    nest

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+
+/**
+ * main - prints out fibonacci numbers
+ * Return:0
+**/
+
+int main(void)
+{
+	int preNum = 0, num = 1;
+
+	while (preNum <= 50)
+	{
+	
+	num += preNum;
+	//fib = num + preNum;
+	printf("%d", fib);
+	printf(",");
+	printf(" ");
+	fib = num 
+	preNum = num;
+	}
+	return (0);
+}
