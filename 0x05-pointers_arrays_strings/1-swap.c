a582809e5686400fb0f0bcf173b790c9bdfab724
Ben <gugben7@gmail.com>
2023-08-21 08:49:28 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - function prints out lowercase of input word
+ * @a: integer pointer
+ * @b: integer pointer
+ * Return: void
+ **/
+
+void swap_int(int *a, int *b)
+{
+	store = *a;
+	*a = *b;
+	*b = store;
+}	
