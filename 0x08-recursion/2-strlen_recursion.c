40c65855a7449b23516119f2190be3e57ba8a0c9
Ben <gugben7@gmail.com>
2023-08-31 07:57:06 -0700

    recursive

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - prints a string in reverse
+ * @s: string to print
+ *  return:return  string legnth
+ */
+
+int _strlen_recursion(char *s)
+{
+
+
+	if (*s == '\0')
+	{
+
+		return (0);
+
+	}
+
+
+	return (1 + _strlen_recursion(s + 1));
+
+
+}
